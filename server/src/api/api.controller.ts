201c7a2f8ea6d390e049a4544d59f3a1e2f2c925
JaJoJi <TheJ0918478513@gmail.com>
2025-07-29 20:43:34 +0700

    [FIX] swap to Bearer token for apikey

--- server/src/api/api.controller.ts
+++ server/src/api/api.controller.ts
@@ -5,18 +5,20 @@ import { ApiKeyGuard } from 'src/auth/strategies/apikeyguard.guard';
 
 @Controller()
 export class ApiController {
-  constructor(private readonly apiService: ApiService) {}
+  constructor(private readonly apiService: ApiService) { }
 
   @UseGuards(ApiKeyGuard)
   @Post('create-agreement')
   async create(
-    @Headers('pactda-api-key') apiKey: string,
+    @Headers('authorization') authHeader: string,
     @Body() body: CreateAgreementDto
   ) {
+    const apiKey = authHeader?.replace('Bearer ', '').trim();
     const contract = await this.apiService.createAgreementContract(body, apiKey);
     return {
       statusCode: HttpStatus.OK,
       data: contract,
     };
   }
+
 }
