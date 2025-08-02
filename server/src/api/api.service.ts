4cdda3ba7a8c4c7e8333b17acc586e8a73356aeb
JaJoJi <TheJ0918478513@gmail.com>
2025-08-03 01:29:59 +0700

    [ADD] fund escrow api(Not finish)

--- server/src/api/api.service.ts
+++ server/src/api/api.service.ts
@@ -7,12 +7,13 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Api } from './api.entity';
-import { SuiClient } from '@mysten/sui.js/dist/cjs/client';
+import { CoinStruct, SuiClient } from '@mysten/sui.js/dist/cjs/client';
 import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 import { CreateAgreementDto } from './dto/create-agreement.dto';
 import { ApiKeyService } from 'src/apikey/api-key.service';
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { ConfigService } from '@nestjs/config';
+import { FundEscrowDto } from './dto/fund-the-escrow.dto';
 
 @Injectable()
 export class ApiService implements OnModuleInit {
@@ -21,9 +22,8 @@ export class ApiService implements OnModuleInit {
     private apiRepository: Repository<Api>,
     @Inject('SUI_CLIENT')
     private readonly suiClient: { client: SuiClient; keypair: Ed25519Keypair },
-    private readonly apiKeyService: ApiKeyService,
     private readonly configService: ConfigService,
-  ) {}
+  ) { }
 
   async findAll(): Promise<Api[]> {
     return this.apiRepository.find({
@@ -94,6 +94,14 @@ export class ApiService implements OnModuleInit {
         tags: null,
         isActive: true,
       },
+      {
+        name: 'FundTheEscrow',
+        path: '/api/fund-the-escrow',
+        method: 'POST',
+        description: 'Add funds to the contract escrow account.',
+        tags: null,
+        isActive: true,
+      },
     ];
     const existingRoutes = await this.apiRepository.find();
 
@@ -130,7 +138,7 @@ export class ApiService implements OnModuleInit {
     const FUNCTION_NAME = this.configService.get<string>('CREATE_AGREEMENT_FUNCTION_NAME');
     const AUTHORITY_ADDRESS = this.configService.get<string>('SUI_AUTHORITY_ADDRESS');
     const CLOCK_ID = this.configService.get<string>('CLOCK_ID') as string;
-    
+
     const tx = new TransactionBlock();
 
     const sanitizedPartyAddresses = dto.partyAddresses.map((addr) => {
@@ -150,6 +158,76 @@ export class ApiService implements OnModuleInit {
         tx.object(CLOCK_ID),
       ],
       typeArguments: [],
+    });
+
+    const result = await this.suiClient.client.signAndExecuteTransactionBlock({
+      signer: this.suiClient.keypair,
+      transactionBlock: tx,
+      options: {
+        showEffects: true,
+        showObjectChanges: true,
+      },
+    });
+
+    return result;
+  }
+
+  async FundEscrow(dto: FundEscrowDto) {
+    const PACKAGE_ID = this.configService.get<string>('PACKAGE_ID');
+    const MODULE_NAME = this.configService.get<string>('MODULE_NAME');
+    const FUNCTION_NAME = this.configService.get<string>('FUND_THE_ESCROW_FUNCTION_NAME');
+    const CLOCK_ID = this.configService.get<string>('CLOCK_ID') as string;
+
+    const coins = await this.suiClient.client.getCoins({
+      owner: this.suiClient.keypair.getPublicKey().toSuiAddress(),
+      coinType: '0x2::sui::SUI',
+    });
+    console.log(coins)
+
+    if (!coins.data.length) {
+      throw new BadRequestException('No SUI coins available in server wallet');
+    }
+
+    const requiredAmount = BigInt(dto.totalCoinMist);
+    let sum = BigInt(0);
+    const coinObjectsForMerge: CoinStruct[] = [];
+
+    for (const coin of coins.data) {
+      sum += BigInt(coin.balance);
+      coinObjectsForMerge.push(coin);
+      if (sum >= requiredAmount) break;
+    }
+    console.log(coinObjectsForMerge)
+
+    if (sum < requiredAmount) {
+      throw new BadRequestException(`Insufficient balance. Available: ${sum.toString()}, Required: ${requiredAmount.toString()}`);
+    }
+
+    const tx = new TransactionBlock();
+
+    const coinInput = tx.mergeCoins(
+      tx.object(coinObjectsForMerge[0].coinObjectId),
+      coinObjectsForMerge.slice(1).map(c => tx.object(c.coinObjectId))
+    );
+
+    const [splitCoins] = tx.splitCoins(coinInput, [tx.pure(dto.totalCoinMist)]);
+
+    if (!splitCoins) {
+      throw new BadRequestException('Failed to split coins. Possibly invalid amount');
+    }
+    console.log('contractId:', dto.contractId);
+    console.log('escrowId:', dto.escrowId);
+    console.log('amountCoin:', [splitCoins], splitCoins);
+    console.log('CLOCK_ID:', CLOCK_ID);
+    tx.moveCall({
+      target: `${PACKAGE_ID}::${MODULE_NAME}::${FUNCTION_NAME}`,
+      arguments: [
+        tx.object(dto.contractId),
+        tx.object(dto.escrowId),
+        tx.object(splitCoins),
+        tx.object(CLOCK_ID),
+      ],
+      typeArguments: ['0x2::sui::SUI'],
     });
 
     const result = await this.suiClient.client.signAndExecuteTransactionBlock({
